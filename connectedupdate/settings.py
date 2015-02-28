36e2dff0825d5120cb7d9974fffbb472a1a6ac99
wersmith <wersmith@gatech.edu>
2015-02-28 12:16:17 -0500

    Update 4

--- connectedupdate/settings.py
+++ connectedupdate/settings.py
@@ -7,106 +7,108 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
+import os
 
-        
+
+HEROKU = True
+
+try:
+    from localflags import *
+except ImportError:
+    pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-try:
-    import os
-
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-
-    # Quick-start development settings - unsuitable for production
-    # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+if HEROKU:
+    # Heroku specific stuff
 
     # SECURITY WARNING: keep the secret key used in production secret!
     # should use os.environ['SECRET_KEY'] in Production
-
     SECRET_KEY = os.environ['SECRET_KEY']
 
-
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
-
-    TEMPLATE_DEBUG = True
-
-    ALLOWED_HOSTS = ['*']
-
-
-    # Application definition
-
-    INSTALLED_APPS = (
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
-        'rest_framework',
-        'userprofile',
-    )
-
-    MIDDLEWARE_CLASSES = (
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    )
-
-    ROOT_URLCONF = 'connectedupdate.urls'
-
-    WSGI_APPLICATION = 'connectedupdate.wsgi.application'
-
-
     # Database
-    # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-
-    # SECURITY WARNING: keep the secret key used in production secret!
-    # should use os.environ['DB_PASSWORD'] in Production
+    # https://docs.djangoproject.com/en/1.7/ref/settings/#databaseson
     import dj_database_url
     DATABASES['default'] =  dj_database_url.config(os.environ['DATABASE_URL'])
 
-    # Internationalization
-    # https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-    LANGUAGE_CODE = 'en-us'
-
-    TIME_ZONE = 'UTC'
-
-    USE_I18N = True
-
-    USE_L10N = True
-
-    USE_TZ = True
-
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-    STATIC_URL = '/static/'
-    STATIC_ROOT = 'staticfiles'
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
 
-    REST_FRAMEWORK = {
-        'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-       'PAGINATE_BY': 10
-    }
-except:
-    import traceback
-    print traceback.format_exc()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'userprofile',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'connectedupdate.urls'
+
+WSGI_APPLICATION = 'connectedupdate.wsgi.application'
+
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
 
 
 try:
