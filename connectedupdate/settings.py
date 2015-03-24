63b640d1f457ba144c4fafe9b3e7c563cde82e60
wersmith <wersmith@gatech.edu>
2015-03-24 01:43:31 -0400

    Shut off debug mode

--- connectedupdate/settings.py
+++ connectedupdate/settings.py
@@ -56,7 +56,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
